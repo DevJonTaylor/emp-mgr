2f169ed7207423d1b4728b10097b178d5a50ba4e
Jon Taylor <jonnytest1101@icloud.com>
2022-03-26 10:18:55 -0500

    Added TODOs

--- src/modals/Department.js
+++ src/modals/Department.js
@@ -1,5 +1,21 @@
 import Modal from './Modal'
 
+/**
+ * TODO:  Create a view for position counts per department
+ * TODO:  Create a view to head count departments
+ * TODO:  Create a view to head count company
+ * TODO:  Create a view to head count managers in department
+ * TODO:  Create a view to head count subordinates in department
+ * TODO:  Create a view to head count managers in company
+ * TODO:  Create a view to head count subordinates in company
+ * TODO:  Create a view to list department employees
+ * TODO:  Create a view to list department managers
+ * TODO:  Create a view to list department subordinates
+ * TODO:  Create a view to list company employees
+ * TODO:  Create a view to list company managers
+ * TODO:  create a view to list company subordinates
+ */
+
 export default class Department extends Modal {
   static table = 'department'
   static keys = ['id', 'name']
--- src/modals/Role.js
+++ src/modals/Role.js
@@ -1,5 +1,10 @@
 import Modal from './Modal'
 
+/**
+ * TODO:  Create a view to Sum Salary for positions
+ * TODO:  Create a view to head count positions
+ */
+
 export default class Role extends Modal {
   static table = 'role'
   static keys = ['id', 'title', 'salary', 'department_id']
